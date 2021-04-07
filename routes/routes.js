f7aefe6a28848911c9caadb224c6d3ace22d5a97
llebedco <78024797+llebedco@users.noreply.github.com>
2021-04-07 13:31:38 -0400

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import express from 'express';
 import sequelize from 'sequelize';
 
@@ -9,100 +10,109 @@ router.get('/', (req, res) => {
   res.send('Welcome to the IMDB API!');
 });
 
-//getting all database records
+// getting all database records
 
-router.get('movie_id', async (req, res) => {
-    try {
-      const movies = await db.inst377_imdb.findAll();
-      const reply = movies.length > 0 ? { data: movies } : { message: 'no results found' };
-      res.json(reply);
-    } catch (err) {
-      console.error(err);
-      res.error('Server error');
-    }
-  });
+router.get('/movie_id', async (req, res) => {
+  try {
+    const movies = await db.inst377_imdb.findAll();
+    const reply = movies.length > 0 ? { data: movies } : { message: 'no results found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
 
 router.get('movie_actors', async (req, res) => {
-    try {
-      const actors = await db.inst377_imdb.findAll();
-      const reply = movies.actors > 0 ? { data: actors } : { message: 'no results found' };
-      res.json(reply);
-    } catch (err) {
-      console.error(err);
-      res.error('Server error');
-    }
-  });
-  router.get('movie_financials', async (req, res) => {
-    try {
-      const financials = await db.inst377_imdb.findAll();
-      const reply = financials.length > 0 ? { data: financials } : { message: 'no results found' };
-      res.json(reply);
-    } catch (err) {
-      console.error(err);
-      res.error('Server error');
-    }
-  });
+  try {
+    const actors = await db.inst377_imdb.findAll();
+    const reply = movies.actors > 0 ? { data: actors } : { message: 'no results found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+router.get('/movie_financials', async (req, res) => {
+  try {
+    const financials = await db.inst377_imdb.findAll();
+    const reply = financials.length > 0 ? { data: financials } : { message: 'no results found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
 
-  router.get('movie_IMDB_ratings', async (req, res) => {
-    try {
-      const ratings = await db.inst377_imdb.findAll();
-      const reply = ratings.length > 0 ? { data: ratings } : { message: 'no results found' };
-      res.json(reply);
-    } catch (err) {
-      console.error(err);
-      res.error('Server error');
-    }
-  });
-  router.get('movie_facebook_likes', async (req, res) => {
-    try {
-      const fb_likes = await db.inst377_imdb.findAll();
-      const reply = fb_likes.length > 0 ? { data: fb_likes } : { message: 'no results found' };
-      res.json(reply);
-    } catch (err) {
-      console.error(err);
-      res.error('Server error');
-    }
-  });
-  router.get('movie_content', async (req, res) => {
-    try {
-      const content = await db.inst377_imdb.findAll();
-      const reply = content.length > 0 ? { data: content } : { message: 'no results found' };
-      res.json(reply);
-    } catch (err) {
-      console.error(err);
-      res.error('Server error');
-    }
-  });
-  router.get('movie_technicals', async (req, res) => {
-    try {
-      const techs = await db.inst377_imdb.findAll();
-      const reply = techs.length > 0 ? { data: techs } : { message: 'no results found' };
-      res.json(reply);
-    } catch (err) {
-      console.error(err);
-      res.error('Server error');
-    }
-  });
-//getting individual elemement 
+router.get('/movie_IMDB_ratings', async (req, res) => {
+  try {
+    const ratings = await db.inst377_imdb.findAll();
+    const reply = ratings.length > 0 ? { data: ratings } : { message: 'no results found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+router.get('/movie_facebook_likes', async (req, res) => {
+  try {
+    const fb_likes = await db.inst377_imdb.findAll();
+    const reply = fb_likes.length > 0 ? { data: fb_likes } : { message: 'no results found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+router.get('/movie_content', async (req, res) => {
+  try {
+    const content = await db.inst377_imdb.findAll();
+    const reply = content.length > 0 ? { data: content } : { message: 'no results found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+router.get('/movie_technicals', async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+  const techs = await db.inst377_imdb.findAll();
+  const reply = techs.length > 0 ? { data: techs } : { message: 'no results found' };
+  res.json(reply);
+});
+// getting individual elemement
 
-  router.get('api/movie_id/3', (req, response) => {
-    try {
-        const movie = await db.inst377_imdb.findAll({
-            where: {
-                movie_id: req_params.movie_id
-        }
+router.get('api/movie_id/3', async (req, res) => {
+  try {
+    const movie = await db.inst377_imdb.findAll({
+      where: {
+        movie_id: req.params.movie_id
+      }
     });
     res.json(movie);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.route ('/movie_technicals')
+  .get(async (req, res) => {
+    try {
+      res.json({message: 'Successfully touched movie technicals'});
+
+    //  await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
     } catch (err) {
-      console.error(err);
-      res.error('Server error');
+      console.error(err)
+      res.json({message: 'Something went wrong'})
     }
+    const techs = await db.inst377_imdb.findAll();
+    const reply = techs.length > 0 ? { data: techs } : { message: 'no results found' };
+    res.json(reply);
   });
-
-        
-  
-  
-  
-
-  
-  
