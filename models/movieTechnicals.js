e9623688dcf97554af6e853cbcdc9b8c09f9c5de
Thomas Marmo <talbertmarmo@gmail.com>
2021-04-15 16:49:33 -0400

    Fixed Formatting of movieTechnicals.js

--- models/movieTechnicals.js
+++ models/movieTechnicals.js
@@ -1,24 +1,24 @@
 export default (sequelize, DataTypes) => {
-    const Technicals = sequelize.define(
-      'movie_technicals',
-      {
-        movie_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          unique: true,
-          primaryKey: true
-        },
-        color: {
-          type: DataTypes.STRING
-        },
-        duration: {
-          type: DataTypes.INTEGER
-        },
-        aspect_ratio: {
-          type: DataTypes.FLOAT
-        }
+  const Technicals = sequelize.define(
+    "movie_technicals",
+    {
+      movie_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
       },
-      { freezeTableName: true, timestamps: false }
-    );
-    return Technicals;
-  };
+      color: {
+        type: DataTypes.STRING,
+      },
+      duration: {
+        type: DataTypes.INTEGER,
+      },
+      aspect_ratio: {
+        type: DataTypes.FLOAT,
+      },
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+  return Technicals;
+};
