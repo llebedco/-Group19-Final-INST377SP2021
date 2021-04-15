c9cbc45f5b88ab6c07ae1ddaf197920c3f29da59
Olivia Dissen <49534719+OliviaDissen01@users.noreply.github.com>
2021-04-15 14:43:14 -0400

    Add files via upload

---
+++ models/yourModel.js
@@ -0,0 +1,28 @@
+export default (sequelize, DataTypes) => {
+  const Movies = sequelize.define(
+    'Movies',
+    {
+      movie_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+        primaryKey: true
+      },
+      movie_title: {
+        type: DataTypes.STRING
+      },
+      director_name: {
+        type: DataTypes.STRING
+      },
+      title_year: {
+        type: DataTypes.INTEGER
+      },
+      country: {
+        type: DataTypes.STRING
+      }
+
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+  return Movies;
+};
