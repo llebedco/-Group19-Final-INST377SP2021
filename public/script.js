faebbd8c80593ec41d37e162be3d09d0af816773
Olivia Dissen <49534719+OliviaDissen01@users.noreply.github.com>
2021-04-21 14:18:41 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,3 +8,14 @@ async function windowActions() {
   const results = await getActor();
   const a = results.data;
 }window.onload = windowActions;
+
+async function getMovie() {
+  const request = await fetch('/api');
+  const moviesData = await request.json();
+  return moviesData;
+}
+
+async function windowActions() {
+  const result = await getMovie();
+  const m = result.data;
+}window.onload = windowActions;
