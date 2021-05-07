27db054bf81ed5355b8792a6eb340072a0690487
llebedco <78024797+llebedco@users.noreply.github.com>
2021-05-06 21:28:23 -0400

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -540,17 +540,5 @@ router.route('/movies/:movie_id')
       res.error('Server error');
     }
   });
-async function getTitle() {
-  const repsonse = await fetch('http://localhost:3000/api/Movies')
-    .then((response) => response.json())
-    .then((movie) => showMovie(movie.results));
-  showMovie = (movie) => {
-    const movieDiv = document.querySelector('#movie_title');
-    movie.forEach((movie) => {
-      const movieElement = document.createElement('p');
-      movieElement.innerText = `Title: ${movie_title.name}`;
-      movieDiv.append(movieElement);
-    });
-  };
-}
+ 
 export default router;
