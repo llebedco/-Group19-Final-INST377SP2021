5d97c82b913cac19e93fc2a0dd6618e5ef8ea09c
llebedco <78024797+llebedco@users.noreply.github.com>
2021-05-11 22:27:47 -0400

    Update script.js
    cleaned up

--- public/script.js
+++ public/script.js
@@ -1,36 +1,3 @@
-// async function getActor() {
-//   const request = await fetch('/api');
-//   const actorsData = await request.json();
-//   return actorsData;
-// }
-
-// async function windowActions() {
-//   const results = await getActor();
-//   const a = results.data;
-// } window.onload = windowActions;
-
-async function getMovie() {
-  const request = await fetch('/api');
-  const moviesData = await request.json();
-  return moviesData;
-}
-
-async function windowActions() {
-  const result = await getMovie();
-  const m = result.data;
-} window.onload = windowActions;
-
-async function getFinance() {
-  const request = await fetch('/api');
-  const financeData = await request.json();
-  return financeData;
-}
-
-async function windowActions() {
-  const result = await getFinance();
-  const f = results.data;
-} window.onload = windowActions;
-
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
